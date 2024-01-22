dac237156c37d737774d817fa5ad2c660483db25
namni22 <142199912+namni22@users.noreply.github.com>
2024-01-22 19:42:37 +0900

    [level 1] Title: 자릿수 더하기, Time: 6.83 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=0;i<String.valueOf(n).length();i++){
+            answer += Integer.parseInt(""+String.valueOf(n).charAt(i));
+        }
+        return answer;
+    }
+}
